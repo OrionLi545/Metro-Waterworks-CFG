f106397353d03cfc056a3d133b2de2bc4fc7983a
Orion Li <haoxuanl@mit.edu>
2021-11-10 08:24:13 +0900

    fixed jumping to cover pag and menu page button

--- js/init.js
+++ js/init.js
@@ -1,6 +1,10 @@
 var lastTime = new Date().getTime();
 var currentTime = new Date().getTime();
-var timeOut = 30 * 1000; //设置超时时间：1分钟
+var timeOut = 60 * 1000; //Time limit to return to cover page: 60000ms
+
+// var test = window.location.pathname;
+// alert(test);
+
 $(function () {
     $(document).mouseover(function () {
         lastTime = new Date().getTime();
@@ -10,8 +14,8 @@ $(function () {
 function toSearchPage() {
     currentTime = new Date().getTime(); 
     if (currentTime - lastTime > timeOut) { 
-        window.parent.location.href ="index.html";//跳转指定页面;
-        //TODO: 不同层级的页面跳转需要一个中转页面
+        window.parent.location.href ="/index.html";//jump back to cover page (index.html)
+        //Need a living server at localhost so that this can work (file:// will not work)
     }
 }   
 window.setInterval(toSearchPage, 1000);
